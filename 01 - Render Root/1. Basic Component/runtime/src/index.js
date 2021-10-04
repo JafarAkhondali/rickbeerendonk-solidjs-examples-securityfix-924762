ad9c71bc082bf65052f8ac6df4d4a62e0974dac1
Rick Beerendonk <rick@beerendonk.com>
2021-10-04 17:20:18 +0200

    Add "Render Root - Basic Component" example.

---
+++ 01 - Render Root/1. Basic Component/runtime/src/index.js
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import { render } from "solid-js/web";
+
+import App from "./app.js";
+
+render(App, document.body);
