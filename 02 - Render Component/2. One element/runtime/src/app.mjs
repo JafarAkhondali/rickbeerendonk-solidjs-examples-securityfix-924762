fb022c5466563f373c8fc157af7e5f4aa2a67893
Rick Beerendonk <rick@beerendonk.com>
2021-10-07 17:49:54 +0200

    Add "Render Component - One Element" example.

---
+++ 02 - Render Component/2. One element/runtime/src/app.mjs
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import html from "solid-js/html";
+
+const App = () => html`<h1>Hello Solid!</h1>`;
+
+export default App;
---
+++ 02 - Render Component/2. One element/runtime/src/index.mjs
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import { render } from "solid-js/web";
+
+import App from "./app.mjs";
+
+render(App, document.body);
