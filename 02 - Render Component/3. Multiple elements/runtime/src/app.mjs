8f499c0f744ffa0f0580bb1b41718e63f5b3d604
Rick Beerendonk <rick@beerendonk.com>
2021-10-16 18:07:43 +0200

    Add "Render Component - Multiple Element" example.

---
+++ 02 - Render Component/3. Multiple elements/runtime/src/app.mjs
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import html from "solid-js/html";
+
+const App = () => html`
+  <>
+      <h1>Hello Solid!</h1>
+      What do you think about Solid?
+      <div>It looks amazing.</div>
+  </>`;
+
+export default App;
